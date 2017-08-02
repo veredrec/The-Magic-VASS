7bf43c14170ea2cd51798ea790ec0db2a4d668d1
rinoma <ganzo.s@libero.it>
2017-08-02 19:00:53 +0300

    Tried to fix some backend stuff

--- src/router.js
+++ src/router.js
@@ -5,11 +5,12 @@ const router = (request, response) => {
 	if (url === '/') {
 		handler.handleHome(response)
 	} else if (url.indexOf('/public/') === 0) {
-		handler.handlePublic(request, response)
+		handler.handlePublic(response, url)
 	} else if (url.indexOf('/?cryptoCurrency=') === 0) {
-		handler.handleInput(request, response, (err, res)=>{
-			console.log(res)
-		})
+		handler.handleInput(request, response)
+		// 	(err, res)=>{
+		// 	console.log(res)
+		// })
 	} else {
 		response.writeHead(404, {'Content-Type' : 'text/html'});
 		response.end('<h1>File not found</h1>')
