a0459aff21ad77f8d4cc0617e0b41df5d3eb917b
rinoma <ganzo.s@libero.it>
2017-08-02 19:41:22 +0300

    Cleared the console logs and other stuff Relates #33

--- public/index.js
+++ public/index.js
@@ -4,18 +4,14 @@ var currencyInput = document.querySelector('.currency')
 var result = document.querySelector('.result')
 
 var makeRequest = function(url) {
-	console.log(123);
 	var XHR = new XMLHttpRequest();
 	XHR.onreadystatechange = function () {
 		if (XHR.readyState === 4 && XHR.status === 200 ) {
-			console.log("Hey!");
-			console.log(XHR.responseText);
-			var xhrObj = JSON.parse(XHR.responseText)  //the response to the request as text
-			showPrice(xhrObj)
-
+			var xhrObj = JSON.parse(XHR.responseText);  //the response to the request as text
+			showPrice(xhrObj);
 		}
 		else if (XHR.readyState === 4 && XHR.status !== 200) {
-			result.innerText = "Fuck we fucked up"
+			result.innerText = "Fuck we fucked up";
 		}
 	}
 	XHR.open('GET', url, true);
@@ -25,27 +21,25 @@ var makeRequest = function(url) {
 
 
 form.addEventListener('submit', function(event) {
-
 	event.preventDefault(); // prevents page from reloading
 	var crypto = cryptoInput.value;
 	var currency = currencyInput.value;
-
 	var url = '?cryptoCurrency=' + crypto + '&currency=' + currency //base url for the parameter in XHR.open
+	// var myVar = setInterval, 1000);
 	makeRequest(url)
+
 	form.reset();
 })
 
 function showPrice(xhrObj) {
-	console.log(xhrObj);
 	var currencyList = document.createElement('ul');
 	currencyList.setAttribute('class', 'pricesUl')
 
-	Object.keys(obj).forEach(function(key) {
+	Object.values(xhrObj).forEach(function(key) {
 		var currencyNode = document.createElement('li');
 		currencyNode.setAttribute('class', 'pricesLi');
-		currencyNode.innerText = key
-		currencyList.appendChild(currencyNode)
+		currencyNode.innerText = key;
+		currencyList.appendChild(currencyNode);
 	})
-
-	// currencyNode.innerText = xhrObj.pricelast; // this could be wrong
+	result.appendChild(currencyList);
 }
--- src/router.js
+++ src/router.js
@@ -1,19 +1,13 @@
 const handler = require('./handler.js');
 
 const router = (request, response) => {
-	console.log(request.url);
 	const url = request.url;
 	if (url === '/') {
-		console.log('home route');
 		handler.handleHome(response)
 	} else if (url.indexOf('/public/') === 0) {
 		handler.handlePublic(response, url)
 	} else if (url.indexOf('/?cryptoCurrency') === 0) {
-		console.log('running');
 		handler.handleInput(request, response)
-		// 	(err, res)=>{
-		// 	console.log(res)
-		// })
 	} else {
 		response.writeHead(404, {'Content-Type' : 'text/html'});
 		response.end('<h1>File not found</h1>')
