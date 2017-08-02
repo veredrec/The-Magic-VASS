ed74fa0f9f180f8b80ac97584cd7235f05e43584
Wyrd00 <amy.tai0120@gmail.com>
2017-08-02 12:24:11 +0300

    Added DOM functions relates to #17

--- public/index.js
+++ public/index.js
@@ -1,9 +1,51 @@
-const tape = require('tape');
-const shot = require('shot');
-const router = require('./router');
+var XHR = new XMLHttpRequest();
+var form = document.querySelector('.form')
+var cryptoInput = document.querySelector('.crypto')
+var currencyInput = document.querySelector('.currency')
+var result = document.querySelector('.result')
 
-test('Initialise', (t) => {
-	let num = 0
-	t.deepEquals(num, 0, 'should return 0')
-	t.end();
-});
+
+XHR.onreadystatechange = function () {
+	if (XHR.readyState === 4 && XHR.status === 200) {
+		var xhrObj = JSON.parse(XHR.responseText)  //the response to the request as text
+		showPrice(xhrObj)
+	
+	} else {
+		result.innerText = "Fucked"
+	}
+}
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault(); // prevents page from reloading
+	var crypto = cryptoInput.value;
+	var currency = currencyInput.value;
+
+	var url = '?cryptoCurrency=' + crypto + '&currency=' + currency //base url for the parameter in XHR.open
+
+	form.reset();
+
+	XHR.open('GET', url, true);
+	XHR.send();
+}) 
+
+function showPrice(xhrObj) {
+	var currencyList = document.createElement('ul');
+	currencyList.setAttribute('class', 'pricesUl')
+
+	Object.keys(obj).forEach(function(key) {
+		var currencyNode = document.createElement('li');
+		currencyNode.setAttribute('class', 'pricesLi');
+		currencyNode.innerText = key
+		currencyList.appendChild(currencyNode)
+	})
+
+
+
+	// currencyNode.innerText = xhrObj.pricelast; // this could be wrong
+	
+
+
+
+}
+	
+
